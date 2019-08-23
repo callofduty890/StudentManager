659c58f0b902b37b6dc867de505da0f5531dfada
callofduty890 <1032031912@qq.com>
2019-08-23 22:49:08 +0800

    添加项目文件。

---
+++ Models/SysAdmin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    /// <summary>
+    /// 管理员权限
+    /// </summary>
+    [Serializable]
+    public class SysAdmin
+    {
+    }
+}
